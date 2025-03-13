3ade4a8410875f1ddbff0a293dc8fbb84227a527
Eugene <eugene@amberpixels.io>
2025-03-13 23:34:40 +0200

    making structToMap public

--- internal/dirtytesting/helpers.go
+++ internal/dirtytesting/helpers.go
@@ -2,11 +2,10 @@ package dirtytesting
 
 import (
 	"encoding/json"
-	"math/rand"
 )
 
-// structToMap converts any struct to a map[string]any via JSON round-trip.
-func structToMap(s any) map[string]any {
+// StructToMap converts any struct to a map[string]any via JSON round-trip.
+func StructToMap(s any) map[string]any {
 	var m map[string]any
 	b, err := json.Marshal(s)
 	if err != nil {
@@ -16,15 +15,4 @@ func structToMap(s any) map[string]any {
 		panic("structToMap: failed to unmarshal struct " + err.Error())
 	}
 	return m
-}
-
-// feelingLucky returns random item from the given slice
-func feelingLucky[T comparable](rng *rand.Rand, s []T) T {
-	if len(s) == 0 {
-		var zero T
-		return zero
-	}
-
-	idx := rng.Intn(len(s))
-	return s[idx]
 }
