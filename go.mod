b947430e8c4b8c04bccd47156cd929f84366d05f
Eugene <eugene@amberpixels.io>
2025-04-28 17:04:36 +0300

    hotfix: dates parsing

--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module github.com/d3rty/json
 go 1.24
 
 require (
-	github.com/amberpixels/years v0.0.2-0.20250313182556-36bdadd14d9a // latest
+	github.com/BurntSushi/toml v1.5.0
+	github.com/amberpixels/years v0.0.4 // latest
 	github.com/stretchr/testify v1.10.0
 )
-
-require github.com/BurntSushi/toml v1.5.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/djherbis/times v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+//replace github.com/amberpixels/years => ../years
---
+++ internal/dirtyjson/dirtytypes_test.go
@@ -0,0 +1,108 @@
+package dirtyjson_test
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/d3rty/json/internal/config"
+	"github.com/d3rty/json/internal/dirtyjson"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestDateTimeUnmarshalJSON tests the DateTime.UnmarshalJSON method.
+func TestDateTimeUnmarshalJSON(t *testing.T) {
+	// Explicitly set global config to the default config
+	// This ensures the test uses the same configuration as the production code
+	config.SetGlobal(func(cfg *config.Config) {
+		// Reset to default config
+		cfg.ResetToDefault()
+	})
+
+	// Test cases
+	testCases := []struct {
+		name     string
+		input    string
+		expected time.Time
+		wantErr  bool
+	}{
+		{
+			name:     "RFC3339 format",
+			input:    `"2023-01-02T15:04:05Z"`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:     "ISO8601 format",
+			input:    `"2023-01-02T15:04:05"`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:     "Unix timestamp as number",
+			input:    `1672671845`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:     "Unix timestamp as string",
+			input:    `"1672671845"`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:     "Unix millisecond timestamp as number",
+			input:    `1672671845000`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:     "Unix millisecond timestamp as string",
+			input:    `"1672671845000"`,
+			expected: time.Date(2023, 1, 2, 15, 4, 5, 0, time.UTC),
+			wantErr:  false,
+		},
+		{
+			name:    "Invalid format",
+			input:   `"not-a-date"`,
+			wantErr: true,
+		},
+		{
+			name:    "Boolean value",
+			input:   `true`,
+			wantErr: true,
+		},
+		{
+			name:    "Object value",
+			input:   `{"date": "2023-01-02"}`,
+			wantErr: true,
+		},
+		{
+			name:    "Array value",
+			input:   `[2023, 1, 2]`,
+			wantErr: true,
+		},
+		{
+			name:     "Null value",
+			input:    `null`,
+			expected: time.Time{},
+			wantErr:  false,
+		},
+	}
+
+	// Run test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var dt dirtyjson.DateTime
+			err := json.Unmarshal([]byte(tc.input), &dt)
+
+			if tc.wantErr {
+				assert.Error(t, err, "Expected error for input %v", tc.input)
+			} else {
+				require.NoError(t, err, "Unexpected error for input %v", tc.input)
+				assert.Equal(t, tc.expected, time.Time(dt), "Expected %v for input %v, got %v", tc.expected, tc.input, time.Time(dt))
+			}
+		})
+	}
+}
